4af43473984c0e658c5a8a7b1b532e72ef2956a1
Samuel Elder <53106453+AkashicRecords@users.noreply.github.com>
2024-05-03 13:37:41 +0700

    Create Configuration.java

---
+++ src/main/java/com/example/config/Configuration.java
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">
+<suite name="API Test Suite" verbose="1">
+    <!-- Define test suite for API testing -->
+    <test name="API Tests">
+        <classes>
+            <!-- Specify the classes that contain your test methods -->
+            <class name="com.example.tests.ApiTests"/>
+        </classes>
+    </test>
+</suite>
